e0622097e9011e0908a0dbf241f1e992f6ceb697
IngLeoCastellanos <ingleonardocastellanos@gmail.com>
2026-05-28 15:21:40 -0500

    avance 4.0

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -7,18 +7,18 @@
         span 3
       h1 Protocolos de servicio en turismo
 
-    .row.justify-content-center.align-items-center.mb-3.bg6
-      .col-lg-5
+    .row.justify-content-center.align-items-center.mb-3.bg6(data-aos='fade-right')
+      .col-lg-5.d-none.d-lg-block
         img.izq(src='@/assets/curso/Tema3/img01.png', alt='')
       .col-lg-7
-        .row
-          .col-lg-2.mb-3
+        .row.p-4
+          .col-lg-2.mb-3.col-4.col-md-2
             img(src='@/assets/curso/Tema3/img02.png', alt='')
           .col-lg-12
             p Los protocolos de servicio en el sector turístico son un conjunto de normas que orientan el comportamiento, la comunicación, la presentación personal y la atención al visitante. Su objetivo es garantizar una experiencia respetuosa, segura, organizada y profesional. No se limitan a la cortesía, sino que abarcan todo el proceso de atención: desde la bienvenida hasta la despedida, considerando las necesidades, cultura, idioma y características del turista.
             p Su importancia radica en que permiten ofrecer un servicio de calidad, mejorar la imagen del destino y generar experiencias positivas, ya que el visitante evalúa tanto el lugar como la atención recibida.
     
-    .bg1.mb-4.p-5
+    .bg1.mb-4.p-5(data-aos='fade-right')
       .row.justify-content-center.align-items-center.fondot3.p-4
         .col-lg-5.mb-3
           h5 Beneficios principales:
@@ -31,7 +31,7 @@
         .col-lg-4.col-md-8
           img(src='@/assets/curso/Tema3/img03.svg', alt='')
 
-    .row.justify-content-center.align-items-center.mb-3
+    .row.justify-content-center.align-items-center.mb-3(data-aos='fade-right')
       .col-lg-9
         .titulo-sexto.color-acento-contenido(data-aos='fade-right')
           h5 Tabla 5.
@@ -85,13 +85,13 @@
                 td #[b Despedida]
                 td Cierre cordial del servicio.
                 td Agradecer la visita e invitar a regresar.
-      .col-lg-3
+      .col-lg-3.d-none.d-lg-block
         img(src='@/assets/curso/Tema3/img04.png', alt='')
 
-    .cajon.color-primario.p-4.mb-4
+    .cajon.color-primario.p-4.mb-4(data-aos='fade-right')
       p Se invita a explorar el video, en el cual se explican de manera clara y práctica los protocolos, normas, técnicas y procedimientos de servicio en el sector turístico, fundamentales para brindar una atención de calidad y mejorar la experiencia del visitante.
 
-    .row.justify-content-center.align-items-center.mb-4           
+    .row.justify-content-center.align-items-center.mb-4(data-aos='fade-right')          
       .col-lg-12
         figure
           .video
@@ -115,8 +115,55 @@
     p El aseo personal, como parte de la urbanidad, es fundamental en el turismo, ya que permite proyectar disciplina, respeto y buena educación (Carreño, 1885/2022). No solo implica mantener una adecuada higiene corporal y presentación, sino también refleja hábitos como el orden, la puntualidad y la responsabilidad en las actividades diarias.
     p Asimismo, el aseo va más allá de lo físico: incluye el comportamiento y la forma de relacionarse con los demás. Actuar con respeto, evitar actitudes o palabras que generen incomodidad y promover una convivencia armoniosa también hacen parte de una buena presentación personal. En este sentido, el cuidado personal no es solo una práctica individual, sino una forma de respeto hacia los demás que contribuye a generar entornos agradables y experiencias turísticas positivas.
 
-    .cajon.color-primario.p-4.mb-4
+    .cajon.color-primario.p-4.mb-4(data-aos='fade-right')
       p En el sector turístico, estos aspectos son clave para transmitir confianza, profesionalismo y calidad en el servicio. Un prestador que cuida su imagen y actitud demuestra compromiso con la experiencia del visitante.
+
+    AcordionA(tipo="a" clase-tarjeta="tarjeta tarjeta--azul")(data-aos='fade-right')
+      .row.justify-content-center.align-items-center(titulo="Aseo personal")
+        .col-lg-5.mb-4.mb-md-0
+          p El aseo personal, como parte de la urbanidad, es fundamental en el turismo, ya que proyecta disciplina, respeto y buena educación (Carreño, 1885/2022). No solo implica higiene corporal y buena presentación, sino también hábitos como el orden, la puntualidad y la responsabilidad en las actividades diarias.
+        .col-lg-6.col-10.col-md-8
+          figure
+            img(src='@/assets/curso/Tema3/img06.png', alt='')
+      .row.justify-content-center.align-items-center(titulo="Orden y método")
+        .col-lg-5.mb-4.mb-md-0
+          h5 Orden y método
+          p Los hábitos de cuidado personal reflejan organización y capacidad para actuar con método. En el turismo, esto se traduce en el cumplimiento de horarios, la preparación adecuada de actividades y la responsabilidad en la prestación del servicio.
+        .col-lg-6.col-10.col-md-8
+          figure
+            img(src='@/assets/curso/Tema3/img07.png', alt='')
+
+      .row.justify-content-center.align-items-center(titulo="Respeto por los demás")
+        .col-lg-5.mb-4.mb-md-0
+          h5 Respeto por los demás
+          p El aseo también implica una actitud considerada hacia los demás. Evitar comportamientos, palabras o actitudes que generen incomodidad contribuye a una convivencia armónica y a una atención más respetuosa hacia el turista.
+        .col-lg-6.col-10.col-md-8
+          figure
+            img(src='@/assets/curso/Tema3/img08.png', alt='')
+
+      .row.justify-content-center.align-items-center(titulo="Imagen profesional")
+        .col-lg-5.mb-4.mb-md-0
+          h5 Imagen profesional
+          p En el sector turístico, el cuidado de la apariencia personal constituye un elemento esencial para generar una percepción positiva en el visitante. Un prestador que proyecta una imagen adecuada transmite seriedad, confianza y compromiso con la excelencia en la prestación del servicio.
+        .col-lg-6.col-10.col-md-8
+          figure
+            img(src='@/assets/curso/Tema3/img09.png', alt='')
+
+      .row.justify-content-center.align-items-center(titulo="Convivencia")
+        .col-lg-5.mb-4.mb-md-0
+          h5 Convivencia
+          p El cuidado personal no es solo un aspecto individual, se convierte en una expresión de respeto hacia quienes comparten el entorno turístico. Una actitud apropiada, el uso de un lenguaje respetuoso y una presentación adecuada contribuyen a generar un clima de armonía y cordialidad entre turistas, comunidad local y prestadores de servicios.
+        .col-lg-6.col-10.col-md-8
+          figure
+            img(src='@/assets/curso/Tema3/img10.png', alt='')
+
+      .row.justify-content-center.align-items-center(titulo="Profesionalismo en el turismo")
+        .col-lg-5.mb-4.mb-md-0
+          h5 Profesionalismo en el turismo
+          p En conjunto, estos elementos fortalecen la calidad del servicio turístico, promueven el respeto mutuo y contribuyen a generar experiencias positivas y memorables para los visitantes.
+        .col-lg-6.col-10.col-md-8
+          figure
+            img(src='@/assets/curso/Tema3/img11.png', alt='')
 
     separador
     #t_3_2.titulo-segundo.mb-5(data-aos="zoom-in-left")
@@ -124,15 +171,67 @@
 
     p La urbanidad es un elemento fundamental en el sector turístico, ya que favorece la convivencia armónica en los destinos. Implica respetar las normas del lugar, valorar la cultura local, cuidar el patrimonio y promover comportamientos responsables entre turistas y comunidad.
 
+    .row.justify-content-start.mb-4(data-aos="zoom-in-left")
+        .col-sm-12.col-lg-7
+          .d-flex.align-items-start
+            img.w-card-icon(src="@/assets/curso/Tema2/ico.svg")
+            h3.bg3.py-2.w-card-text.text-white Parámetros de urbanidad
 
+    .row.justify-content-center.align-items-stretch.mb-4.bg6(data-aos='fade-right')
+      .col-lg-7.align-items-center.p-5
+        br
+        br
+        p De acuerdo con Manuel Antonio Carreño, la urbanidad es el conjunto de reglas que orientan la forma correcta de actuar y comunicarse en sociedad. Estas normas permiten relacionarse con dignidad, respeto y consideración, reflejando valores como la prudencia, la moderación y la benevolencia.
+        p Una persona con urbanidad sabe comportarse según el contexto, cuidando sus palabras, gestos, presentación personal y acciones. Además, desarrolla el “tacto social”, es decir, la capacidad de actuar con oportunidad y respeto en cada situación, adaptándose a diferentes personas y culturas.
+      .col-lg-5.d-flex
+        img.w-100.h-100.izq2.d-none.d-lg-block(src='@/assets/curso/Tema3/img12.png', alt='', style='object-fit: cover;')
 
+    .row.justify-content-center.align-items-center.mb-4(data-aos='fade-right')
+      .col-lg-4.mb-3
+        p En el turismo, estos parámetros se reflejan en:
+        p.mb-0  • Presentación personal adecuada. 
+        p.mb-0  • Puntualidad y orden. 
+        p.mb-0  • Comunicación respetuosa. 
+        p.mb-0  • Prudencia en el trato. 
+        p.mb-0  • Conductas que no afecten la comodidad del visitante.
+      .col-lg-8.bg7.p-5
+        .row.justify-content-center
+          .col-lg-1.col-4.mb-3.col-md-2
+            img(src="@/assets/curso/Tema3/img13.png")
+          .col-lg-11
+            p Su aplicación permite proyectar una imagen positiva del destino, la empresa y la comunidad anfitriona.
+      
+    .row.justify-content-start.mb-4(data-aos="zoom-in-left")
+        .col-sm-12.col-lg-7
+          .d-flex.align-items-start
+            img.w-card-icon(src="@/assets/curso/Tema2/ico.svg")
+            h3.bg3.py-2.w-card-text.text-white Normas de cortesía
 
+    .row.justify-content-center.align-items-center.mb-3(data-aos='fade-right')
+      .col-lg-12
+        img(src="@/assets/curso/Tema3/img14.png")
+    p La cortesía es la expresión práctica de la urbanidad y se manifiesta en el trato amable y respetuoso hacia los demás. No se limita a fórmulas sociales, sino que implica consideración, paciencia, tolerancia y disposición para mantener relaciones armoniosas.
 
-
-
-
-
-
+    .row.justify-content-center.align-items-center.mb-4(data-aos='fade-right')
+      .col-lg-4.mb-3
+        p En el contexto turístico, la cortesía se evidencia en acciones como:
+        p.mb-0  • Saludar y dar la bienvenida cordialmente. 
+        p.mb-0  • Escuchar con atención y responder con paciencia. 
+        p.mb-0  • Brindar información clara y oportuna. 
+        p.mb-0  • Respetar el espacio y las diferencias del visitante. 
+        p.mb-0  • Agradecer la visita y despedirse de manera amable. 
+      .col-lg-8.bg7.p-5
+        .row.justify-content-center
+          .col-lg-1.col-4.mb-3.col-md-2
+            img(src="@/assets/curso/Tema3/img15.png")
+          .col-lg-11
+            p Asimismo, implica evitar comportamientos como interrumpir, elevar la voz, incomodar o ridiculizar al turista.
+    .row.justify-content-start.mb-4(data-aos="zoom-in-left")
+        .col-sm-12.col-lg-7
+          .d-flex.align-items-start
+            img.w-card-icon(src="@/assets/curso/Tema2/ico.svg")
+            h3.bg3.py-2.w-card-text.text-white Importancia en el turismo
+    p La aplicación de la urbanidad y la cortesía fortalece la calidad del servicio, mejora la experiencia del visitante y promueve el respeto mutuo. Estos principios permiten generar confianza, proyectar profesionalismo y contribuir a experiencias turísticas positivas y memorables (Carreño, 1885/2022).
 
 
 
